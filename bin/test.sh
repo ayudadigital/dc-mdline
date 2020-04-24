85b63b173d3960cd7dbb860ef36efc85f28dcab2
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-04-24 15:59:09 +0200

    Upgrade: Change tpbtools and TIC para Bien references with Ayuda Digital

--- bin/test.sh
+++ bin/test.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 cd "$(dirname $0)/../test"
-docker run -v $(pwd)/:/workspace tpbtools/dc-mdline:beta example.md | diff example.html -
+docker run -v $(pwd)/:/workspace ayudadigital/dc-mdline:beta example.md | diff example.html -
